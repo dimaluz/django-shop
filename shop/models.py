1e267ff12250895cceefc79cb44811054fee3110
dimaluz <dimaluz@rambler.ru>
2024-07-03 15:47:35 +0100

    Product detail and Product list views were added

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -9,11 +10,15 @@ class Category(models.Model):
         indexes = [
             models.Index(fields=['name']),
         ]
-        verbouse_name = 'category'
-        verbouse_name_plural = 'categories'
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", args=[self.slug])
+    
     
 
 class Product(models.Model):
@@ -37,3 +42,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.id, self.slug])
+    
